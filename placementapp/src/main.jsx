a26ea995225c9dbeac880c69786a1cff387cef36
Karthikbodapati <karthikreddybodapati@gmail.com>
2026-03-05 13:09:26 +0530

    main.jsx is modified

--- placementapp/src/main.jsx
+++ placementapp/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'mdbootstrap/css/mdb.min.css';
-import 'mdbootstrap/js/mdb.min.js';
+import 'mdb-ui-kit/css/mdb.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById("root")).render(
