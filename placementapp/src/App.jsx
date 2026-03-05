973a109727b58190ac87603dda6765637778f36d
Harshavardhan <harshavardhan46724@gmail.com>
2026-03-06 05:09:07 +0530

    Half part of code playground is done

---
+++ placementapp/src/App.jsx
@@ -0,0 +1,33 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Layout/Dashboard";
+import Course from "./pages/Course";
+import ExamLeaderboard from "./pages/ExamLeaderboard";
+import ExamReports from "./pages/ExamReports";
+import Exams from "./pages/Exams";
+import Jobs from "./pages/Jobs";
+import Logout from "./pages/Logout";
+import Playground from "./pages/Playground";
+import PlaygroundDetail from "./pages/PlaygroundDetail";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<Profile />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="course" element={<Course />} />
+          <Route path="exams" element={<Exams />} />
+          <Route path="exam-reports" element={<ExamReports />} />
+          <Route path="exam-leaderboard" element={<ExamLeaderboard />} />
+          <Route path="playground" element={<Playground />} />
+          <Route path="playground/python" element={<PlaygroundDetail />} />
+          <Route path="logout" element={<Logout />} />
+
+        </Route>
+      </Routes>
+  );
+}
+
+export default App;
