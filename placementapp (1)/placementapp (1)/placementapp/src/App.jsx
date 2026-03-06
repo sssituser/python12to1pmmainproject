bac9381238f9ae46e5b31eb7efe5bae847a64540
Harshavardhan <harshavardhan46724@gmail.com>
2026-03-06 18:29:18 +0530

    exam list is added

---
+++ placementapp (1)/placementapp (1)/placementapp/src/App.jsx
@@ -0,0 +1,41 @@
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Layout/Dashboard";
+import Course from "./pages/Course";
+import ExamLeaderboard from "./pages/ExamLeaderboard";
+import ExamReports from "./pages/ExamReports";
+import Exams from "./pages/Exams";
+import ExamsList from "./pages/ExamsList";
+import Jobs from "./pages/Jobs";
+import Logout from "./pages/Logout";
+import Playground from "./pages/Playground";
+
+import PlaygroundDetail from "./pages/PlaygroundDetail";
+import LeaveRequest from "./pages/Leaverequest";
+
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+      <Routes>
+        {/* Standalone exam page without sidebar */}
+        <Route path="/exam" element={<Exams />} />
+        
+        {/* All other routes with Dashboard layout */}
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<Profile />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="course" element={<Course />} />
+          <Route path="exams" element={<ExamsList />} />
+          <Route path="exam-reports" element={<ExamReports />} />
+          <Route path="exam-leaderboard" element={<ExamLeaderboard />} />
+          <Route path="playground" element={<Playground />} />
+          <Route path="/playground/:language" element={<PlaygroundDetail />} />
+          <Route path="logout" element={<Logout />} />
+          <Route path="leave-request" element={<LeaveRequest />} />
+        </Route>
+      </Routes>
+  );
+}
+
+export default App;
