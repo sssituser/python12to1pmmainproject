8f2b75df0a25f73ecbcfa083bb7011042a48364d
karanjivikas <karanjivikas03@gmail.com>
2026-03-10 08:43:59 +0530

    exam backend code added

--- placement/myapp/serializers.py
+++ placement/myapp/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Exam, ExamAttempt  # add Exam, ExamAttempt here
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = '__all__'
+
+
+class ExamAttemptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExamAttempt
+        fields = ['id', 'status', 'attempted_at']
+
+
+class ExamSerializer(serializers.ModelSerializer):
+    attempt = ExamAttemptSerializer(read_only=True)
+    start_time = serializers.TimeField(format="%I:%M %p")
+    end_time = serializers.TimeField(format="%I:%M %p")
+
+    class Meta:
+        model = Exam
+        fields = [
+            'id', 'title', 'start_date', 'start_time',
+            'end_time', 'duration_minutes', 'is_finished',
+            'attempt', 'created_at'
+        ]
--- placement/myapp/urls.py
+++ placement/myapp/urls.py
@@ -3,4 +3,7 @@ from myapp.views import *
 
 urlpatterns = [
     path('login/', login_view, name='login'),
+    path('exams/finished/', FinishedExamListView.as_view(), name='finished-exams'),
+    path('exams/<int:pk>/attempt/', UpdateAttemptView.as_view(), name='update-attempt'),
+
 ]
