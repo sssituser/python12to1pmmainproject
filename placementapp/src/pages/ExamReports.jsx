748dd605194a0566c8cf741e35dca4719bb70cc0
pithaniharshavardhan-coder <pithaniharshavardhan4900@gmail.com>
2026-03-13 15:40:26 +0530

    DailyExam updated

--- placementapp/src/pages/ExamReports.jsx
+++ placementapp/src/pages/ExamReports.jsx
@@ -6,42 +6,45 @@ const navigate = useNavigate();
 
 return (
 
-            <div className="container mt-5">
-              <div className="row">
-                <div className="col-md-3">
-                  <div className="card text-center">
-                      <h5>Daily Exam</h5>
-                      <button
-                      className="btn btn-success"
-                      onClick={() => navigate("/daily-exams")}
-                      >
-                      View
-                      </button>
-                  </div>
-                  </div>
+<div className="container mt-5">
+  <div className="row">
 
-            <div className="col-md-3">
-                    <div className="card text-center">
-                    <h5>Weekly Exam</h5>
-                    <button className="btn btn-secondary">
-                    Coming Soon
-                    </button>
-                    </div>
-            </div>
+    <div className="col-md-3">
+      <div className="card text-center p-3 shadow">
+        <h5>Daily Exam</h5>
 
-            <div className="col-md-3">
-                <div className="card text-center">
-                <h5>Monthly Exam</h5>
-                <button className="btn btn-secondary">
-                Coming Soon
-                </button>
-                </div>
-            </div>
+        <button
+          className="btn btn-success"
+          onClick={() => navigate("/dashboard/daily-exams")}
+        >
+          VIEW
+        </button>
 
-            </div>
-            </div>
-            );
+      </div>
+    </div>
 
+    <div className="col-md-3">
+      <div className="card text-center p-3 shadow">
+        <h5>Weekly Exam</h5>
+        <button className="btn btn-secondary">
+          COMING SOON
+        </button>
+      </div>
+    </div>
+
+    <div className="col-md-3">
+      <div className="card text-center p-3 shadow">
+        <h5>Monthly Exam</h5>
+        <button className="btn btn-secondary">
+          COMING SOON
+        </button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+);
 }
 
 export default ExamReports;
