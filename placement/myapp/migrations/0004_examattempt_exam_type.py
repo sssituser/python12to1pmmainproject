52592029f44afebf3ef32a3b733dcbbcae8db5ce
pithaniharshavardhan-coder <pithaniharshavardhan4900@gmail.com>
2026-03-21 15:44:10 +0530

    Backend changes

--- placement/myapp/migrations/0004_examattempt_exam_type.py
+++ placement/myapp/migrations/0004_examattempt_exam_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myapp', '0003_merge_20260320_1928'),
+        ('myapp', '0003_merge_20260320_1730'),
     ]
 
     operations = [
